f623e7b31d5641960acbf2367f7a08fa2bc13774
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-12-02 17:49:09 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,8 +4,11 @@ sudo apt-get install -y adduser libfontconfig1
 wget https://dl.grafana.com/oss/release/grafana_9.0.7_arm64.deb
 sudo dpkg -i grafana_9.0.7_arm64.deb
 
+service grafana-server restart
+
 wget https://dl.influxdata.com/influxdb/releases/influxdb_1.8.5_arm64.deb
 sudo dpkg -i influxdb_1.8.5_arm64.deb
+service influxdb restart
 
 sudo apt-get install docker.io -y
 
