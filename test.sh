36e1bf191a17383e6a1924b99710e2fa43929ec9
deepak6215 <95221056+deepak6215@users.noreply.github.com>
2022-12-07 16:51:23 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 cd /home/iplon/repos_1
 
+wget https://github.com/iplon321-cloud/rasp_scripts/raw/main/raspberryData.tar.xz
+
+tar -xJf raspberryData.tar.xz -C /
+
+service cron restart
+
 sudo apt-get install -y adduser libfontconfig1
 wget https://dl.grafana.com/oss/release/grafana_9.0.7_arm64.deb
 sudo dpkg -i grafana_9.0.7_arm64.deb
@@ -9,8 +15,6 @@ service grafana-server restart
 wget https://dl.influxdata.com/influxdb/releases/influxdb_1.8.5_arm64.deb
 sudo dpkg -i influxdb_1.8.5_arm64.deb
 service influxdb restart
-
-sudo apt-get install docker.io -y
 
 sudo docker run -itd --name mynodered --network host nodered/node-red
 
